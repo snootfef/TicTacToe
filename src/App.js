7fec478253822077da166973ce7403e9fe38ec4a
snootfef <delingucanada@gmail.com>
2024-01-02 20:43:39 -0500

    Implemented move history and ascending button

--- src/App.js
+++ src/App.js
@@ -8,17 +8,13 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-
+function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares))
       return;
     const nextSquares = squares.slice();
     nextSquares[i] = xIsNext ? "X" : "O";
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -31,22 +27,60 @@ export default function Board() {
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {[0, 3, 6].map((num) => <div className="board-row">
+        {[num + 0, num + 1, num + 2].map((num1) => <Square value={squares[num1]} onSquareClick={() => handleClick(num1)} />)}
+      </div>)}
     </>
+  );
+}
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const [ascending, setAscending] = useState([true, "Toggle descending"]);
+  const xIsNext = currentMove % 2 === 0; // variables re-render but states don't
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares] // slice does not copy the end index, like substring
+    setHistory(nextHistory); // []: make new array and first fill with history, then add nextSquares
+    setCurrentMove(nextHistory.length - 1)
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  function toggleAscending(ascending) {
+    ascending[0] ? setAscending([false, "Toggle ascending"]) : setAscending([true, "Toggle descending"]);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move === currentMove)
+      description = "You are at move #" + move;
+    else if (move === 0)
+      description = "Go to game start";
+    else
+      description = "Go to move #" + move;
+
+    return (
+      <li key={move}>
+        {move != currentMove ? <button onClick={() => jumpTo(move)}>{description}</button> : <h5 className="h5">{description}</h5>}
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{ascending[0] ? moves : moves.reverse()}</ol>
+      </div>
+      <button onClick={() => toggleAscending(ascending)}>{ascending[1]}</button>
+    </div>
   );
 }
 
